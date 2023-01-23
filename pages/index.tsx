2ac4b0ad589838ebc82a22515d823b11ce4fa993
ghaieth96 <ghaiethbmoussa@gmail.com>
2023-01-23 14:57:45 +0100

    cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import Head from "next/head";
 import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
-// import ManulHeader from "../components/ManualHeader";
 import Header from "../components/Header";
 import LotteryEntrance from "../components/LotteryEntrance";
 
@@ -17,9 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      {/* <main className={styles.main}> */}
       <LotteryEntrance />
-      {/* </main> */}
     </>
   );
 }
